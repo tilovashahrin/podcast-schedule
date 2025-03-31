8ebe38d264c76e401924b9fe6259dec794533654
Tilova Shahrin <tilova97@gmail.com>
2025-03-31 08:52:46 -0400

    change mock event in test

--- tests/unit/useCalendar.spec.ts
+++ tests/unit/useCalendar.spec.ts
@@ -1,18 +1,30 @@
 import { describe, it, expect, vi } from 'vitest';
-import { useCalendar } from '../../src/composables/fullCalendar'
+import { useCalendar } from '../../src/composables/fullCalendar';
 import { ref } from 'vue';
 
 describe('useCalendar composable', () => {
+  const mockEvent = [
+    {
+      start: new Date(),
+      end: new Date(),
+      title: 'Event',
+      description: 'event description',
+      backgroundColor: '#ff0000',
+      textColor: '#ffffff',
+    },
+  ];
+
   it('should initialize with default values', () => {
-    const { calendarOptions, modalVisible, selectedEvent } = useCalendar([]);
+    const { calendarOptions, modalVisible, selectedEvent } = useCalendar(mockEvent);
 
     expect(modalVisible.value).toBe(false);
     expect(selectedEvent.value).toEqual({ title: '', description: '' });
     expect(calendarOptions.value.initialView).toBe('dayGridMonth');
+    expect(calendarOptions.value.events).toEqual(mockEvent);
   });
 
   it('should handle event click and show modal with event details', () => {
-    const { modalVisible, selectedEvent, calendarOptions } = useCalendar([]);
+    const { modalVisible, selectedEvent, calendarOptions } = useCalendar(mockEvent);
 
     //simulate event click
     const event = {
@@ -36,8 +48,27 @@ describe('useCalendar composable', () => {
     });
   });
 
+  it('should handle event click when description is missing', () => {
+    const { modalVisible, selectedEvent, calendarOptions } = useCalendar(mockEvent);
+
+    const event = {
+      event: {
+        title: 'Event Without Description',
+        extendedProps: {},
+      },
+    };
+
+    calendarOptions.value.eventClick(event);
+
+    expect(modalVisible.value).toBe(true);
+    expect(selectedEvent.value).toEqual({
+      title: 'Event Without Description',
+      description: 'No description available.',
+    });
+  });
+
   it('should close modal when closeModal is called', () => {
-    const { modalVisible, closeModal } = useCalendar([]);
+    const { modalVisible, closeModal } = useCalendar(mockEvent);
 
     //simulate event clicks
     modalVisible.value = true;
